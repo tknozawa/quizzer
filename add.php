0e898b772c63eabb2358ee1b043cedf9cd35b001
Takapons <nonotaka888@gmail.com>
2019-11-12 22:57:50 +0900

    Completed

--- add.php
+++ add.php
@@ -1,3 +1,54 @@
+<?php include 'database.php'; ?>
+<?php
+
+  if(isset($_POST['submit'])){
+
+    $question_number = $_POST['question_number'];
+    $question_text = $_POST['question_text'];
+    $correct_choice = $_POST['correct_choice'];
+
+    $choices = array();
+    $choices[1] = $_POST['choice1'];
+    $choices[2] = $_POST['choice2'];
+    $choices[3] = $_POST['choice3'];
+    $choices[4] = $_POST['choice4'];
+    $choices[5] = $_POST['choice5'];
+
+    $query = "INSERT INTO questions (question_number,text)
+            VALUES('$question_number', '$question_text')";
+    $insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+    if($insert_row){
+      foreach($choices as $choice => $value){
+        if($value != ''){
+          if($correct_choice == $choice){
+            $is_correct = 1;
+          } else {
+            $is_correct = 0;
+          }
+          $query = "INSERT INTO choices (question_number, is_correct, text)
+              VALUES ('$question_number', '$is_correct', '$value')";
+
+          $insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+          if($insert_row){
+            continue;
+          } else {
+            die('Error : ('.$mysqli->errno. ') '. $mysqli->error);
+          }
+        }
+      }
+      $msg = 'Question has been added';
+    }
+  }
+
+  $query = "SELECT * FROM questions";
+  $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+  $total = $result->num_rows;
+  $next = $total+1;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,10 +67,16 @@
   <main>
     <div class="container">
       <h2>Add A Question</h2>
+      <?php
+        if(isset($msg)){
+          echo '<p>'.$msg.'</p>';
+        }
+      ?>
+
       <form action="add.php" method="post">
         <p>
           <label for="">Question Number: </label>
-          <input type="number" name="question_number" />
+          <input type="number" value="<?php echo $next; ?>" name="question_number" />
         </p>
         <p>
           <label for="">Question Text: </label>
